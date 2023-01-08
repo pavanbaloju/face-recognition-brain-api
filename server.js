b098dbf4e5e465bb6f4cf64bd80938412f4bd69e
Pavan Baloju <pavanbaloju9@gmail.com>
2023-01-08 11:26:32 +0530

    Add profile and image endpoints

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.use(express.json());
 const database = {
     users: [
         {
-            id: 123,
+            id: '123',
             name: 'Surya',
             email: 'surya@sky.com',
             password: 'iam360',
@@ -15,7 +15,7 @@ const database = {
             joined: new Date()
         },
         {
-            id: 124,
+            id: '124',
             name: 'Hardik',
             email: 'hardik@cap.com',
             password: 'iamcap',
@@ -26,24 +26,24 @@ const database = {
 }
 
 server.get('/', (req, res) => {
-    res.json(database.users);
+    return res.json(database.users);
 })
 
 server.post('/signin', (req, res) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     database.users.forEach(user => {
-        if(user.email === email && user.password === password){
-            res.status(200).json("Login sucess")
+        if (user.email === email && user.password === password) {
+            return res.status(200).json("Login sucess")
         }
     });
-    res.status(404).json("Error loggin in");
+    return res.status(404).json("Error loggin in");
 })
 
 server.post('/register', (req, res) => {
-    const {email, password, name} = req.body;
+    const { email, password, name } = req.body;
     const users = database.users;
     const newUser = {
-        id: 125,
+        id: '125',
         name: name,
         email: email,
         password: password,
@@ -51,8 +51,29 @@ server.post('/register', (req, res) => {
         joined: new Date()
     };
     users.push(newUser)
-    res.status(200).json(users[users.length - 1]);
-}) 
+    return res.status(200).json(users[users.length - 1]);
+})
+
+server.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    const filtered = database.users.filter(user => user.id === id);
+    if(filtered.length){
+        return res.status(200).json(filtered[0]);
+    } else {
+        return res.status(404).json("User not found");
+    }
+})
+
+server.put('/image', (req, res) => {
+    const { id } = req.body;
+    const filtered = database.users.filter(user => user.id === id);
+    if(filtered.length){
+        return res.status(200).json(++filtered[0].entries);
+    } else {
+        return res.status(404).json("User not found");
+    }
+})
+
 
 server.listen(3010, () => {
     console.log('server is running on port 3010');
